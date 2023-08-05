8852184a8c1994d2e5e5c4093c07a2b697d9dcb7
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-08-05 04:39:45 -0500

    Added files with basics of PWA

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { validateDidVp } from "./api/api";
+import "./App.css";
+
+const App = () => {
+    const [url, setUrl] = useState("joe.com"); // Url for did doc creation
+
+    const [isOrg, setIsOrg] = useState(false); // flag for if the user is an organization or not
+
+    const [orgName, setOrgName] = useState(""); // name of the organization
+    const [firstName, setFirstName] = useState("joe"); // first name of the user
+    const [lastName, setLastName] = useState("brandon"); // last name of the user
+
+    const [vpUrl, setVpUrl] = useState("https://aus36.github.io/didweb-doc/vp.json"); // Url for hosted vp
+    const [didUrl, setDidUrl] = useState("https://aus36.github.io/didweb-doc/did.json"); // Url for hosted did document
+
+    const [loaded, setLoaded] = useState(false); // flag for when the validate pair function has been called
+
+    const [validateSuccessful, setValidateSuccessful] = useState(false); // flag for the result when the validate pair function has been called
+
+    const setType = () => {
+        if (document.getElementById('type').value === 'organization') {
+        setIsOrg(true);
+        } else {
+        setIsOrg(false);
+        }
+    }
+
+    return ( 
+        <div className="main-container">
+            <h1>did:web Wallet PWA</h1>
+            <div id="didDocGen" className="didDocGen">
+                <form
+                onSubmit={e => {
+                    // e.preventDefault(); // interacts with server, keep commented out for now
+                    // submit(url, isOrg, orgName, firstName, lastName);
+                }}>
+                <label>
+                    URL:
+                    <input type="text" value={url} onChange={e => setUrl(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Subject Type:
+                    <select id='type' onChange={setType}>
+                    <option value="person">Person</option>
+                    <option value="organization">Organization</option>
+                    </select>
+                </label>
+                <br />
+                {isOrg ?
+                    <label>
+                    Organization Name:
+                    <input type="text" onChange={e => setOrgName(e.target.value)} />
+                    </label> :
+                    <label>
+                    First Name:
+                    <input id="firstName" value={firstName} type="text" onChange={e => setFirstName(e.target.value)} />
+                    <br />
+                    Last Name:
+                    <input id="lastName" value={lastName} type="text" onChange={e => setLastName(e.target.value)} />
+                    </label>
+                }
+                <br />
+                <input type="submit" value="Submit" />
+                </form>
+            </div>
+            <div id="validation" className="validation">
+                <form>
+                    <label>
+                        DID Document URL:
+                        <input id="didDocUrl" value={didUrl} type="text" onChange={e => setDidUrl(e.target.value)} />
+                        <br />
+                        Verifiable Presentation URL:
+                        <input id="vpUrl" value={vpUrl} type="text" onChange={e => setVpUrl(e.target.value)} />
+                    </label>
+                </form>
+                <br />
+                <button onClick={ () => {setValidateSuccessful(validateDidVp(vpUrl, didUrl)); setLoaded(true);}}>Validate did document and vp pair</button>
+                <br />
+                {loaded
+                ? <p>Validation result: {validateSuccessful ? "Pair Sucessfully Validated" : "Invalid DID Doc/VP Pair"}</p> 
+                : <p>Nothing to validate yet</p>}
+            </div>
+        </div>
+    );
+    }
+
+export default App;
---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+// Functions for validating the did document and vp pair
+//========================================================
+// 1. Fetch the did document from a hosted location
+async function fetchJSON(url) {
+    return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(JSON.stringify(data, null, 2)); // Prints the JSON to the console
+      return data;
+    })
+    .catch(error => console.error('An error occurred while fetching the JSON from '+url+": ", error));
+  }
+  
+  // 2. Validate the did document and vp pair
+  async function validateDidVp(vpUrl, didUrl) {
+    let vp = await fetchJSON(vpUrl);
+    let did = await fetchJSON(didUrl);
+    
+    if(vp["verifiableCredentials"][0]["credentialSubject"]["id"] = did["id"]) {
+      return true
+    }
+    else return false
+  }
+  //========================================================
+
+export { validateDidVp };
